c45ae2eeb00827205f9dfdd27fca1ef97a1bae35
petrutlucian94 <petrutlucian94@gmail.com>
2013-08-29 20:56:42 +0300

    VHDX maximum internal size

--- vhdx_real_size.py
+++ vhdx_real_size.py
@@ -30,46 +30,46 @@ def get_max_internal_size(path, size):
     except IOError:
         print "Unable to get data from the VHDX file: ", path
 
-def get_current_header_offset(path, handler):
+def get_current_header_offset(file_handler):
     sequence_number=[]
     for offset in HEADER_OFFSETS:
-        handler.seek(offset + 8)
-        sequence_number.append(struct.unpack('<Q', handler.read(8))[0])
+        file_handler.seek(offset + 8)
+        sequence_number.append(struct.unpack('<Q', file_handler.read(8))[0])
     current_header = sequence_number.index(max(sequence_number))
     return HEADER_OFFSETS[current_header]
 
-def get_log_size(path, handler):
-    current_header_offset = get_current_header_offset(path, handler)
+def get_log_size(file_handler):
+    current_header_offset = get_current_header_offset(file_handler)
     offset = current_header_offset * 1024 + LOG_LENGTH_OFFSET
-    handler.seek(offset) 
-    log_size = struct.unpack('<I', handler.read(4))[0]
+    file_handler.seek(offset) 
+    log_size = struct.unpack('<I', file_handler.read(4))[0]
     return log_size
 
-def get_metadata_size_and_offset(path, handler):
+def get_metadata_size_and_offset(file_handler):
     offset = METADATA_FILE_OFFSET + REGION_TABLE_OFFSET * 1024
-    handler.seek(offset)
-    metadata_offset = struct.unpack('<Q', handler.read(8))[0]
-    metadata_size = struct.unpack('<I', handler.read(4))[0]
+    file_handler.seek(offset)
+    metadata_offset = struct.unpack('<Q', file_handler.read(8))[0]
+    metadata_size = struct.unpack('<I', file_handler.read(4))[0]
     return metadata_size, metadata_offset
 
-def get_block_size(path, handler):
-    metadata_offset = get_metadata_size_and_offset(path, handler)[1]
+def get_block_size(file_handler):
+    metadata_offset = get_metadata_size_and_offset(file_handler)[1]
     offset = metadata_offset + 48 
-    handler.seek(offset)
-    file_parameter_offset = struct.unpack('<I', handler.read(4))[0]
+    file_handler.seek(offset)
+    file_parameter_offset = struct.unpack('<I', file_handler.read(4))[0]
     
-    handler.seek(file_parameter_offset + metadata_offset)
-    block_size = struct.unpack('<I', handler.read(4))[0]
+    file_handler.seek(file_parameter_offset + metadata_offset)
+    block_size = struct.unpack('<I', file_handler.read(4))[0]
     return block_size
 
-def get_logical_sector_size(path, handler):
-    metadata_offset = get_metadata_size_and_offset(path, handler)[1]
-    offset = metadata_offset + 48 + 32 * 3
-    handler.seek(offset)
-    logical_sector_offset = struct.unpack('<I', handler.read(4))[0]
+def get_logical_sector_size(file_handler):
+    metadata_offset = get_metadata_size_and_offset(file_handler)[1]
+    offset = metadata_offset + 144
+    file_handler.seek(offset)
+    logical_sector_offset = struct.unpack('<I', file_handler.read(4))[0]
 
-    handler.seek(logical_sector_offset + metadata_offset)
-    logical_sector_size = struct.unpack('<I', handler.read(4))[0]
+    file_handler.seek(logical_sector_offset + metadata_offset)
+    logical_sector_size = struct.unpack('<I', file_handler.read(4))[0]
     return logical_sector_size
 
 '''print "max internal size: ", get_max_internal_size("c:\\cirros.vhdx", 1024 ** 3)'''
