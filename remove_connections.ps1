c7a478f72a3680bbcd91a7e039961e725398d04f
Lucian Petrut <lpetrut@cloudbasesolutions.com>
2017-04-13 13:15:00 +0300

    Remove old stuff

--- remove_connections.ps1
+++
@@ -1,7 +0,0 @@
-$sessions = iscsicli.exe SessionList | where {$_ -match "(Session Id *: .*)"}
-ForEach($session in $sessions){
-	$line = $session | where {$_ -match "(Session Id *: )(?<session_id>.*)"}
-	$id = $matches.session_id
-	echo "Removing session $id"
-	iscsicli.exe logouttarget $id
-}
